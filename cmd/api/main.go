84de79877f4ae2394733b750a7519996b393876c
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2024-04-04 23:52:15 +0530

    Demo httpadapter/ServeMuxErr router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"errors"
+	"go-http-error-adapter/internal/httpadapter"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("GET /health_check", func(w http.ResponseWriter, r *http.Request) {})
+	router := httpadapter.NewServeMuxErr(func(hfe httpadapter.HandlerFuncErr) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			if err := hfe(w, r); err != nil {
+				log.Printf("Encountered err: %s", err)
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}
+	})
+
+	router.HandleFunc("GET /health_check", func(w http.ResponseWriter, r *http.Request) {})
+
+	router.HandleFuncErr("GET /error_check", func(w http.ResponseWriter, r *http.Request) error {
+		return errors.New("Error check")
+	})
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
 	log.Println("listening on localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("cannot start server: %s", err)
 	}
 }
